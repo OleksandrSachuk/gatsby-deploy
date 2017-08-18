02c9fc715fbbaae0c8686d5d57098eb0f479cc58
Oleksandr Sachuk <ovsachuk@gmail.com>
2017-08-18 10:14:20 +0300

    nav modify

--- src/layouts/nav/nav.js
+++ src/layouts/nav/nav.js
@@ -3,62 +3,61 @@ import Link from 'gatsby-link'
 import {Nav, NavItem} from 'react-bootstrap'
 import './style.css'
 
-export default class Navigation  extends Component{
-  constructor(props){
+export default class Navigation extends Component {
+  constructor(props) {
     super(props);
 
     this.state = {
-      activeKey: 1
+      home: true,
+      photos: false,
+      contacts: false
     };
 
-    this.handleSelect = this.handleSelect.bind(this);
+    this.handleClickNav = this.handleClickNav.bind(this);
   }
 
-  handleSelect(selectedKey) {
-    this.setState({activeKey: selectedKey});
+  handleClickNav(item) {
+    if (item === 'home') {
+      // this.setState(home: true);
+    }
+    const allStateItem = Object.keys(this.state);
+    allStateItem.map(key => {
+      if (this.state[key] && key === item) {
+        return
+      } else if (key === item) {
+        this.setState({[key]: true});
+      } else {
+        this.setState({[key]: false});
+      }
+    });
   }
 
-  render(){
-    const {activeKey} = this.state;
+  render() {
+    const {home, photos, contacts} = this.state;
     return (
       <div className="container">
-        <Nav bsStyle="tabs" activeKey={activeKey} onSelect={this.handleSelect}>
-          <NavItem eventKey={1} title="Home">
-            <Link
-              to="/"
-              style={{
-                color: '#000080',
-                textDecoration: 'none',
-                padding: '10px 15px'
-              }}
-            >
-              Home
-            </Link>
-          </NavItem>
-          <NavItem eventKey={2} title="Photos">
-            <Link
-              to="/photos"
-              style={{
-                color: '#000080',
-                textDecoration: 'none',
-                padding: '10px 15px'
-              }}
-            >
-              Photos
-            </Link>
-          </NavItem>
-          <NavItem eventKey={3} title="Contacts">
-            <Link
-              to="/contacts/"
-              style={{
-                color: '#000080',
-                textDecoration: 'none',
-                padding: '10px 15px'
-              }}
-            >
-              Contacts
-            </Link>
-          </NavItem>
+        <Nav bsStyle="tabs">
+          <Link
+            to="/"
+            className={home ? 'nav-link active': 'nav-link'}
+            onClick={this.handleClickNav.bind(this, 'home')}
+          >
+            Home
+          </Link>
+          <Link
+            to="/photos/"
+            className={photos ? 'nav-link active': 'nav-link'}
+            onClick={this.handleClickNav.bind(this, 'photos')}
+          >
+            Photos
+          </Link>
+          <Link
+            to="/contacts/"
+            className={contacts ? 'nav-link active': 'nav-link'}
+            onClick={this.handleClickNav.bind(this, 'contacts')}
+          >
+            Contacts
+          </Link>
         </Nav>
       </div>
     )
